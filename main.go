54fdc06581803b7edd9f061d20aac22dcf0c81c1
tzipp <bettercode@fastmail.com>
2018-05-15 21:37:19 -0400

    Print before/after.

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 	"path"
 	"regexp"
 	"log"
+	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
 	src := args[0]
-	_, file := path.Split(src)
+	dir, file := path.Split(src)
 
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dst := reg.ReplaceAllString(file, "-")
+	dstFile := reg.ReplaceAllString(file, "-")
+	parts := []string{dir, dstFile}
+	dst := strings.Join(parts, "")
 
+	// os.Rename(src, dst)
+	fmt.Println(src)
 	fmt.Println(dst)
 }
