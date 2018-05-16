c062421d8065c8ce825f7b556fd81d6afabdefba
tzipp <bettercode@fastmail.com>
2018-05-16 10:50:45 -0400

    Need some debugging for windows/network issues.

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"regexp"
 	"log"
 	"strings"
+	"fmt"
 )
 
 func main() {
 	args := os.Args[1:]
 	src := args[0]
 	dir, file := path.Split(src)
+	fmt.Println("Directory: ", dir)
+	fmt.Println("Source file: ", file)
 
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
 	if err != nil {
@@ -21,6 +24,8 @@ func main() {
 	dstFile := reg.ReplaceAllString(file, "-")
 	parts := []string{dir, dstFile}
 	dst := strings.Join(parts, "")
+
+	fmt.Println("Destination: ", dst)
 
 	os.Rename(src, dst)
 }
